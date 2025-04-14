98cd8938fa1a9d2bc46374fbe270ecb02a6f4772
romankochkov <roman.kochkov@hotmail.com>
2025-04-14 03:24:32 -0400

    Added redirect processing for api route

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://136.59.129.136:33457/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
