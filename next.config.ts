e7d0216d27dd31aacb3e87e059015ae9180e21e2
romankochkov <roman.kochkov@hotmail.com>
2025-05-09 18:21:52 -0400

    Changed config, new render rule

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
+import TerserPlugin from "terser-webpack-plugin";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config, { dev, isServer }) {
+    if (!dev && !isServer) {
+      config.optimization.minimizer = [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true, // Удаляет все console.* (log, warn, error)
+            },
+          },
+        }),
+      ];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
