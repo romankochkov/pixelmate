3253ec7ccb04adf1a2ff2de67de77dab332b7f04
romankochkov <roman.kochkov@hotmail.com>
2025-05-09 22:27:14 -0400

    Changed config

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,11 @@
 import type { NextConfig } from "next";
-import TerserPlugin from "terser-webpack-plugin";
 
 const nextConfig: NextConfig = {
-  webpack(config, { dev, isServer }) {
-    if (!dev && !isServer) {
-      config.optimization.minimizer = [
-        new TerserPlugin({
-          terserOptions: {
-            compress: {
-              drop_console: true, // Удаляет все console.* (log, warn, error)
-            },
-          },
-        }),
-      ];
-    }
-    return config;
+  compiler: {
+    removeConsole:
+      process.env.NODE_ENV === "production"
+        ? { exclude: ["error"] }
+        : false,
   },
 };
 
